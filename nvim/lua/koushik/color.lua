8d1fb354262ffed811ecbd7555390836a4821a16
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-06-12 01:13:02 +0600

    line number color change

--- nvim/lua/koushik/color.lua
+++ nvim/lua/koushik/color.lua
@@ -11,3 +11,15 @@ require("themery").setup({
     livePreview = true
 })
 
+
+
+-- Change the color of absolute line numbers
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888", bg = "NONE" })
+
+-- Change the color of the current line number
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFD700", bold = true })
+
+-- (Optional) If you use relative numbers and want different colors
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#5f87af" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#5f87af" })
+
