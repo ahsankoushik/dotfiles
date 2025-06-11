a5018c272dc227e670d05468ce6d4dc8dab531ca
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-06-12 01:02:26 +0600

    transparent bg for ghostty and nvim

--- nvim/lua/koushik/color.lua
+++ nvim/lua/koushik/color.lua
@@ -1,5 +1,10 @@
-vim.cmd.colorscheme "catppuccin"
 
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true
+})
+
+vim.cmd.colorscheme "catppuccin"
 
 require("themery").setup({
     themes={"catppuccin", "kanagawa", "rose-pine", "tokyonight"},
