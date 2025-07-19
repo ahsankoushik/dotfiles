5eed0452b71a613a06ad9ddf4ac8d164067fbd93
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-07-19 19:14:05 +0600

    kulala config added to remap
    having some trouble with kulala.nvim's default conf so added my own

--- .config/nvim/lua/koushik/remap.lua
+++ .config/nvim/lua/koushik/remap.lua
@@ -56,3 +56,9 @@ vim.keymap.set('n', '<S-H>', ':vertical resize -10<CR>')
 vim.keymap.set('n', '<S-L>', ':vertical resize +10<CR>')
 vim.keymap.set('n', '<S-J>', ':resize -10<CR>')
 vim.keymap.set('n', '<S-K>', ':resize +10<CR>')
+
+-- kulala config
+local kula_la = require("kulala")
+vim.keymap.set("n", "<leader>Rs", kula_la.run , {desc = "kulala send"})
+vim.keymap.set("n", "<leader>Ra", kula_la.run_all , {desc = "kulala send all"})
+vim.keymap.set("n", "<leader>Rr", kula_la.run_all , {desc = "kulala reply last request"})
