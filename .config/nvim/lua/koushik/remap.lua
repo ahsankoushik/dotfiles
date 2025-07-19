1f8c6ece6ca713f5c23681af359f13c63b9c1b31
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-07-19 19:33:01 +0600

    patch replay

--- .config/nvim/lua/koushik/remap.lua
+++ .config/nvim/lua/koushik/remap.lua
@@ -6,14 +6,14 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tag' })
 vim.keymap.set('n', '<leader>fd', function()
-    builtin.find_files({ hidden = true})
-end, { desc = "Telescope find hidden files too"})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = "Git files"})
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Git Commits"})
-vim.keymap.set('n', '<leader>gs', builtin.git_status,{ desc = "Git Status"})
+    builtin.find_files({ hidden = true })
+end, { desc = "Telescope find hidden files too" })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = "Git files" })
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Git Commits" })
+vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = "Git Status" })
 -- vim.keymap.set('i', '<C-z>' , vim.cmd.undo, { desc = "Undo" })
 
-vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' }) -- Normal mode
+vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' })                     -- Normal mode
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { desc = 'Save file (insert mode)' }) -- Insert mode
 
 
@@ -23,31 +23,31 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 
 -- split screen
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Virtial Split"})
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Horizonal Split"})
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Split Justificcation"})
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close Split Screen"})
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Virtial Split" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Horizonal Split" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Split Justificcation" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close Split Screen" })
 
 
--- tabs 
-vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew, { desc = "New tab"})
-vim.keymap.set("n", "<leader>tl", vim.cmd.tabn, { desc = "tab next"})
-vim.keymap.set("n", "<leader>th", vim.cmd.tabp, { desc = "tab previous"})
-vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose, { desc = "tab close"})
+-- tabs
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew, { desc = "New tab" })
+vim.keymap.set("n", "<leader>tl", vim.cmd.tabn, { desc = "tab next" })
+vim.keymap.set("n", "<leader>th", vim.cmd.tabp, { desc = "tab previous" })
+vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose, { desc = "tab close" })
 
 
 -- git
 vim.keymap.set("n", "<leader>gt", "<cmd>LazyGit<cr>", { desc = "Git Explorer" })
 
--- themery 
-vim.keymap.set("n", "<leader>tm", "<cmd>Themery<CR>", { desc = "Themery"})
+-- themery
+vim.keymap.set("n", "<leader>tm", "<cmd>Themery<CR>", { desc = "Themery" })
 
 vim.g.copilot_enabled = false
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', {
-  silent = true,
-  expr = true,
-  script = true,
+    silent = true,
+    expr = true,
+    script = true,
 })
 
 
@@ -61,4 +61,4 @@ vim.keymap.set('n', '<S-K>', ':resize +10<CR>')
 local kula_la = require("kulala")
 vim.keymap.set("n", "<leader>Rs", kula_la.run , {desc = "kulala send"})
 vim.keymap.set("n", "<leader>Ra", kula_la.run_all , {desc = "kulala send all"})
-vim.keymap.set("n", "<leader>Rr", kula_la.run_all , {desc = "kulala reply last request"})
+vim.keymap.set("n", "<leader>Rr", kula_la.replay , {desc = "kulala reply last request"})
