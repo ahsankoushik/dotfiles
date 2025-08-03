6e6afd7e547e7c7a721bc4a674c0d43556da8cb5
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-08-04 03:38:36 +0600

    may be i dont need the title area

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,8 @@ config.font_size = 13
 
 config.enable_tab_bar = false
 
-config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
+-- config.window_decorations = "TITLE | RESIZE"
 
 -- config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 5
