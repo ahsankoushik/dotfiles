54f949a4ab2d54649fd2e1cc5496aea8e263f45e
ahsankoushik <ahsankoushik@gmail.com>
2026-09-12 09:17:47 +0600

    auto reload

---
+++ .config/nvim/lua/koushik/auto_reload.lua
@@ -0,0 +1,6 @@
+-- Auto-reload files when changed externally
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  pattern = "*",
+  command = "checktime"
+})
